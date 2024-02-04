bf031f1c5e8dd40114e634ec90d72d9d7f74fe82
Dmitry Gritsenko <k01419q45@ya.ru>
2024-02-05 02:35:37 +0400

    feat: new mobile logic 'n' stuff

--- src/utils/isMobile.ts
+++
@@ -1,7 +0,0 @@
-export const isMobile = () => {
-    const body = document.body;
-    const width = body.clientWidth;
-    const height = body.clientHeight;
-
-    return width < Math.max(height, 450);
-};
