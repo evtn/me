155e4d8d655d102578501736aa66bc74859361ed
Dmitry Gritsenko <k01419q45@ya.ru>
2024-02-05 04:29:32 +0400

    fix: memoize timeline build

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,4 +1,5 @@
 import { useAtom } from "jotai";
+import { useMemo } from "preact/hooks";
 import { JSXInternal } from "preact/src/jsx";
 
 import { Card } from "@/components";
@@ -17,38 +18,42 @@ export const Timeline = () => {
     const [filters] = useAtom(timelineFiltersAtom);
     const { sortedEntries, futureEdge } = useTimeline();
 
-    const timelinePieces: JSXInternal.Element[] = [];
-    let futureCardsVisible = 0;
+    const timelinePieces: JSXInternal.Element[] = useMemo(() => {
+        let futureCardsVisible = 0;
+        const timelinePieces: JSXInternal.Element[] = [];
 
-    sortedEntries.forEach((data, i) => {
-        const entryType = data.type || "project";
-        const cardVisible = filters[entryType];
+        sortedEntries.forEach((data, i) => {
+            const entryType = data.type || "project";
+            const cardVisible = filters[entryType];
 
-        timelinePieces.push(
-            <Card key={i} data={data} isVisible={cardVisible} />,
-        );
-
-        if (i === futureEdge) {
             timelinePieces.push(
-                <span
-                    key="edge"
-                    className={classBuilder("timeline-future-edge")
-                        .color("text")
-                        .build(classname.card)}
-                >
-                    Plans
-                </span>,
+                <Card key={i} data={data} isVisible={cardVisible} />,
             );
+
+            if (i === futureEdge) {
+                timelinePieces.push(
+                    <span
+                        key="edge"
+                        className={classBuilder("timeline-future-edge")
+                            .color("text")
+                            .build(classname.card)}
+                    >
+                        Plans
+                    </span>,
+                );
+            }
+
+            if (i > futureEdge && cardVisible) {
+                futureCardsVisible++;
+            }
+        });
+
+        if (!futureCardsVisible) {
+            timelinePieces.splice(futureEdge + 1, 1);
         }
 
-        if (i > futureEdge && cardVisible) {
-            futureCardsVisible++;
-        }
-    });
-
-    if (!futureCardsVisible) {
-        timelinePieces.splice(futureEdge + 1, 1);
-    }
+        return timelinePieces;
+    }, [sortedEntries, futureEdge, filters]);
 
     return (
         <div
